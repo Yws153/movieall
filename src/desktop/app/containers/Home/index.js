7eaf4b8a1af5f5d0e846362fcd0ab0041f174c9d
叶蔚珊 <1435443048@qq.com>
2018-06-06 19:37:41 +0800

    不要

--- src/desktop/app/containers/Home/index.js
+++ src/desktop/app/containers/Home/index.js
@@ -20,6 +20,81 @@ class Home extends React.Component {
 		const panes = homeState.get('panes')
 		const homeActiveKey = homeState.get('homeActiveKey')
 
+		const a = {}
+
+		const b = {}
+
+		const comparison = (a, b) => {
+
+			console.log(typeof a);
+			console.log(typeof b);
+
+			const typeOfOne = typeof a
+			const typeOfTwo = typeof b
+
+			let result = {}
+
+			if (typeOfOne === typeOfTwo) { // 传入的操作数的类型相同
+
+				if (typeOfOne === 'undefined' || typeOfOne === 'boolean' || typeOfOne === 'number' || typeOfOne === 'string') { // 类型为 Null 或者 其他任何其他对象
+					// 除 null 的基础类型 直接对比值
+					if (a === b) {
+						return result = {
+							match: true,
+							message: ''
+						}
+					} else {
+						return result = {
+							match: false,
+							message: `${a} 与 ${b} 不匹配`
+						}
+					}
+				} else if (typeOfOne === 'symbol') {
+					return result = {
+						match: false,
+						message: `symbol 未做匹配`
+					}
+				} else if (typeOfOne === 'function') {
+					return result = {
+						match: false,
+						message: `function 未做匹配`
+					}
+				} else if (typeOfOne === 'object') {
+					if (a !== null && b !== null) {
+
+
+
+
+
+
+					} else {
+						if (a === b) {
+							return result = {
+								match: true,
+								message: ''
+							}
+						} else {
+							return result = {
+								match: false,
+								message: `${a} ${b}类型不匹配`
+							}
+						}
+					}
+				}
+
+			} else { // 第一层的类型不同
+				result = {
+					match: false,
+					message: `${typeOfOne} ${typeOfTwo}类型不匹配`
+				}
+				return result
+			}
+		}
+
+		const result = comparison(a, b)
+
+		console.log(result);
+
 		return (
 			<div>
 				<ul>
@@ -35,11 +110,9 @@ class Home extends React.Component {
 					onChange={(key) => dispatch(homeActions.addHomeTabpane(key))}
 					activeKey={homeActiveKey}
 					onEdit={(targetKey, action) => {
-
 						if (action === 'remove') {
 							dispatch(homeActions.removeHomeTabpane(targetKey))
 						}
-
 					}}>
 					{panes.map(v => {
 						const Component = v.get('key') === 'Home' ? v.get('content') : asyncComponent(`Home/${v.get('key')}`)
